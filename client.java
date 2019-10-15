2be2fb48acf8e7bebdeac1847ad8829bf59622d7
Mlhozgr <mlhozgr@gmail.com>
2019-10-15 23:31:34 +0300

    Update client.java

--- client.java
+++ client.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class client{
 
 public static void main(String[] args) throws IOException{
-	Socket s = new Socket("localhost",80);
+	Socket s = new Socket("66.249.64.254",80);
 	PrintWriter pr=new PrintWriter(s.getOutputStream());
 	pr.println("Merhabalar");
 	pr.flush();
